d31829da79d1d5c41f00c51621fb1056d08cf691
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-04-02 10:49:54 +0200

    Add DRY

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 
 
 def home_page(request):
-	return render(request, "index.html")
+	titleIndex = "Wynajem i zarządzanie apartamentami w Polsce"
+	# django_rendered_doc = "<H1>{{title}}</H1>".format(title=titleIndex)
+	return render(request, "index.html", {"tittle": titleIndex})
 
 def about_page(request):
-	return HttpResponse("<h1>Wynajem i zarządzanie apartamentami\O nas\</h1>")
+	return render(request, "about.html", {"tittle": "O nas"})
 
 def contact_page(request):
-	return HttpResponse("<h1>Wynajem i zarządzanie apartamentami\Kontakt</h1>")
+	return render(request, "index.html", {"tittle": "Kontakt"})
